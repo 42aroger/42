f5cb4ee319e9a638677b69163eb5351f7d888f88
Antoine ROGER <aroger@e1r2p10.42.fr>
2019-03-13 17:27:24 +0100

    abc

---
+++ 42.1/Algo/libft/ft_strsplit.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aroger <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/13 17:02:19 by aroger            #+#    #+#             */
+/*   Updated: 2019/03/13 17:05:02 by aroger           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+static int	ft_count_words(char const *s, char c)
+{
+	int		i;
+	int		word;
+
+	i = 0;
+	word = 0;
+	while (*s)
+	{
+		if (word == 0 && *s != c)
+		{
+			word = 1;
+			i++;
+		}
+		else if (word == 1 && *s == c)
+			word = 0;
+		s++;
+	}
+	return (i);
+}
+
+static int	ft_string_len(char const *s, char c)
+{
+	int		len;
+
+	len = 0;
+	while (*s != '\0' && *s != c)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+char	**ft_strsplit(char const *s, char c)
+{
+	char	**array;
+	int		nbrwords;
+	int		index;
+
+	if (s)
+	{
+		index = 0;
+		nbrwords = ft_count_words(s, c);
+		array = (char**)malloc(sizeof(char *) * (nbrword + 1));
+		if (!array)
+			return (NULL);
+		while (nbrwords--)
+		{
+			while (*s != '\0' && *s != c)
+				s++;
+		}
+	}
+}
