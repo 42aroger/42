7103e7cfe9042e1dcee73ba8e6bb346e8f9fe62b
42aroger <aroger@student.42.fr>
2019-03-23 15:29:12 +0100

    fix fill of \0

--- 42.1/Algo/libft/ft_strncpy.c
+++ 42.1/Algo/libft/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: aroger <aroger@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 09:38:17 by aroger            #+#    #+#             */
-/*   Updated: 2019/03/12 15:55:59 by aroger           ###   ########.fr       */
+/*   Updated: 2019/03/23 15:28:26 by aroger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,6 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 	size_t	i;
 
 	i = 0;
-	if (ft_strlen(src) == 0)
-	{
-		dst[i] = src[i];
-		i++;
-		while (dst[i] != '\0')
-			dst[i++] = '\0';
-		return (dst);
-	}
 	while (len-- > 0 && src[i])
 	{
 		dst[i] = src[i];
